0022457ba8a6a978e5be8562107fe1349c5dff46
thewordgirl <rebecca.broughton@outlook.com>
2025-08-11 12:08:34 -0500

    Adding Background + Changing Title A Bit

--- guitest.py
+++ guitest.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import PhotoImage, font
 
 def update_label():
     user_input = entry.get()
@@ -7,9 +8,20 @@ def update_label():
 #comment for github test
 
 root = tk.Tk()
+root.geometry("500x200") #set window size
+
+#frame
+#frame = tk.Frame(root, width = 500, height = 200, bg = "lightblue")
+#frame.pack()
+
+#background
+backgroundImage = PhotoImage(file='BackgroundImagePy.png')
+backgroundLavel = tk.Label(root, image=backgroundImage)
+backgroundLavel.place(relwidth=1, relheight=1) #stretch to fill
+
 root.title("Basic GUI Example")
 
-label = tk.Label(root, text="Enter something: ")
+label = tk.Label(root, text="Centered Title", font = ("Times New Roman", 16, "bold"))
 label.pack(pady=10)
 
 entry = tk.Entry(root)
@@ -18,4 +30,5 @@ entry.pack(pady=10)
 button = tk.Button(root, text="Update Label", command=update_label)
 button.pack(pady=10)
 
+#run the application
 root.mainloop()
