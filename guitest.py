0d41225bbb34e2b1291340750bb7ce30d8e07954
thewordgirl <rebecca.broughton@outlook.com>
2025-08-13 10:59:53 -0500

    A little more touch ups!
    :D

--- guitest.py
+++ guitest.py
@@ -1,59 +1,57 @@
 import tkinter as tk
 import subprocess
 from tkinter import PhotoImage, font
+from tkinter.font import Font
 
 root = tk.Tk()
-root.geometry("500x145") #set window size
+root.geometry("500x160") #set window size
 root.resizable(False, False)
 
+#BAH Colors
+strongCyan = "#00a5b5"
+electricGreen = "#bad63a"
+darkCyan = "#00838F"
+magenta = "#e55ed6"
+lightCyan = "#23d2d7"
+
+
 #background
-backgroundImage = PhotoImage(file='BackgroundImagePy.png')
+backgroundImage = PhotoImage(file='for_the_slide_show.png')
 backgroundLavel = tk.Label(root, image=backgroundImage)
 backgroundLavel.place(relwidth=1, relheight=1) #stretch to fill
 
+
 root.title("Automation GUI")
 
-label = tk.Label(root, text="Automation Scripts", font = ("Times New Roman", 16, "bold"))
+label = tk.Label(root, text="Automation Scripts", font = ("Constantia", 16, "bold"), fg = darkCyan, bg="white",borderwidth = 0, highlightthickness=0)
 label.pack(pady=15)
 
 #scripts
 def runAutomationScript():
-   subprocess.run(['./automation.sh'])
+  subprocess.run(['./automation.sh'])
 
 def runDestroyScript():
    subprocess.run(['./destroy.sh'])
 
+
 #buttons
-button1 = tk.Button(root, 
-                    text="Range Creation",
-                    font = ("Times New Roman", 12),
-                    bg = "white",
-                    fg = "darkblue",
-                    width = 10,
-                    height = 1,
-                    padx = 20,
-                    pady = 1,
-                    relief = "raised",
+image1 = PhotoImage(file = "buttonCreate.png")
+
+createButton = tk.Button(root, 
+                    image = image1,
                     command = runAutomationScript
                     )
-button1.pack(pady=10)
-button1.place(x=65, y = 80)
+createButton.pack(pady=10)
+createButton.place(x=30, y = 65)
 
+image2 = PhotoImage(file = "destroyButton.png")
 
-button2 = tk.Button(root, 
-                    text="Range Destruction",
-                    font = ("Times New Roman", 12),
-                    bg = "white",
-                    fg = "darkblue",
-                    width = 10,
-                    height = 1,
-                    padx = 20,
-                    pady = 1,
-                    relief = "raised",
+destroyButton = tk.Button(root, 
+                    image = image2,
                     command = runDestroyScript
                     )
-button2.pack(pady=10)
-button2.place(x=300, y = 80)
+destroyButton.pack(pady=10)
+destroyButton.place(x=270, y = 65)
 
 #run the application
 root.mainloop()
